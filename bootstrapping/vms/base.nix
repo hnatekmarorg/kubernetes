652e3552c8385e6dbbac0160635885e432fd1598
Hnatekmar <m.hnatma@gmail.com>
2024-08-26 21:28:41 +0200

    Update base.nix

--- bootstrapping/vms/base.nix
+++ bootstrapping/vms/base.nix
@@ -1,6 +1,10 @@
 { config, ... }:
 
 {
+  environment.systemPackages = with pkgs; [
+    vim
+    fzf
+  ];
   users.users."root".openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDFPly2MYEeh4FtFtftOa0qasGW4VNIzYv/ZzheQ/dFs martin@fedora" ];
   
   services.openssh = {
