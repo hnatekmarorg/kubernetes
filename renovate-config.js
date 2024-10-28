8c815c4bceaac13144a7d3ee38e772f46e950356
Hnatekmar <m.hnatma@gmail.com>
2024-10-28 21:12:13 +0100

    Update renovate-config.js

---
+++ renovate-config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  username: 'renovate-release',
+  gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
+  onboarding: false,
+  platform: 'github',
+  forkProcessing: 'enabled',
+  dryRun: 'full',
+  repositories: ['hnatekmarorg/kubernetes'],
+  packageRules: [
+    {
+      description: 'lockFileMaintenance',
+      matchUpdateTypes: [
+        'pin',
+        'digest',
+        'patch',
+        'minor',
+        'major',
+        'lockFileMaintenance',
+      ],
+      dependencyDashboardApproval: false,
+      minimumReleaseAge: 0,
+    },
+  ],
+};
